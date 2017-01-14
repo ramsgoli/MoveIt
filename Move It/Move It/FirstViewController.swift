e66ba52cfbd2589c7a556b4da9b6c52d66024428
Lawrence Chen <lawrencechen98@gmail.com>
2017-01-13 20:25:51 -0800

    started xcode proj

---
+++ Move It/Move It/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Move It
+//
+//  Created by Lawrence Chen on 1/13/17.
+//  Copyright © 2017 Lawrence Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
